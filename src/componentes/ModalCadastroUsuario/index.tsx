dcd83cac9d8bafd84ab73cb511fce6d1c3a978c8
natcurti <naticurti01@gmail.com>
2024-06-06 19:35:13 -0300

    Include modal in the base project

--- src/componentes/ModalCadastroUsuario/index.tsx
+++ src/componentes/ModalCadastroUsuario/index.tsx
@@ -1,88 +1,59 @@
-import { AbBotao, AbCampoTexto, AbModal } from "ds-alurabooks"
-import { useState } from "react"
-
-import imagemPrincipal from './assets/login.png'
-
-import './ModalCadastroUsuario.css'
+import { AbBotao, AbCampoTexto, AbModal } from "ds-alurabooks";
+import imageForm from "./assets/login.png";
+import { useState } from "react";
+import "./ModalCadastroUsuario.css";
 
 const ModalCadastroUsuario = () => {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [complemento, setComplemento] = useState("");
+  const [cep, setCep] = useState("");
+  const [senha, setSenha] = useState("");
+  const [senhaConfirmada, setSenhaConfirmada] = useState("");
 
-    const [nome, setNome] = useState('')
-    const [email, setEmail] = useState('')
-    const [endereco, setEndereco] = useState('')
-    const [complemento, setComplemento] = useState('')
-    const [cep, setCep] = useState('')
-    const [senha, setSenha] = useState('')
-    const [senhaConfirmada, setSenhaConfirmada] = useState('')
-
-    const aoSubmeterFormular = (evento: React.FormEvent<HTMLFormElement>) => {
-        evento.preventDefault()
-        const usuario = {
-            nome,
-            email,
-            senha,
-            endereco,
-            cep,
-            complemento
-        }
-        console.log(usuario)
-        alert('Usuário foi cadastrado com sucesso!')
-    }
-
-    return (<AbModal 
-        titulo="Cadastrar" 
-        aberta={true}
-        aoFechar={() => console.log('fecha ai')}    
+  return (
+    <AbModal
+      titulo="Cadastrar"
+      aberta={true}
+      aoFechar={() => {
+        console.log("Fechar");
+      }}
     >
-        <section className="corpoModalCadastro">
-            <figure>
-                <img src={imagemPrincipal} alt="Pessoa segurando uma chave na frente de uma tela de computador que está exibindo uma fechadura" />
-            </figure>
-            <form onSubmit={aoSubmeterFormular}>
-                <AbCampoTexto 
-                    label="Nome"
-                    value={nome}
-                    onChange={setNome}
-                />
-                <AbCampoTexto 
-                    label="E-mail"
-                    value={email}
-                    onChange={setEmail}
-                    type="email"
-                />
-                <AbCampoTexto 
-                    label="Endereço"
-                    value={endereco}
-                    onChange={setEndereco}
-                />
-                <AbCampoTexto 
-                    label="Complemento"
-                    value={complemento}
-                    onChange={setComplemento}
-                />
-                <AbCampoTexto 
-                    label="CEP"
-                    value={cep}
-                    onChange={setCep}
-                />
-                <AbCampoTexto 
-                    label="Senha"
-                    value={senha}
-                    onChange={setSenha}
-                    type="password"
-                />
-                <AbCampoTexto 
-                    label="Confirmação da senha"
-                    value={senhaConfirmada}
-                    onChange={setSenhaConfirmada}
-                    type="password"
-                />
-                <div className="acoes">
-                    <AbBotao texto="Cadastrar"/>
-                </div>
-            </form>
-        </section>
-    </AbModal>)
-}
+      <div className="corpo-modal">
+        <figure>
+          <img
+            src={imageForm}
+            alt="Monitor com uma fechadura e uma pessoa com uma chave logo ao lado"
+          />
+        </figure>
+        <form>
+          <AbCampoTexto value={nome} label="Nome" onChange={setNome} />
+          <AbCampoTexto value={email} label="Email" onChange={setEmail} />
+          <AbCampoTexto
+            value={endereco}
+            label="Endereço"
+            onChange={setEndereco}
+          />
+          <AbCampoTexto
+            value={complemento}
+            label="Complemento"
+            onChange={setComplemento}
+          />
+          <AbCampoTexto value={cep} label="CEP" onChange={setCep} />
+          <AbCampoTexto value={senha} label="Senha" onChange={setSenha} />
+          <AbCampoTexto
+            value={senhaConfirmada}
+            label="Confirme sua senha"
+            onChange={setSenhaConfirmada}
+          />
+          <footer>
+            <AbBotao texto="Cadastrar" />
+          </footer>
+        </form>
+      </div>
+    </AbModal>
+  );
+};
 
-export default ModalCadastroUsuario
+export default ModalCadastroUsuario;
