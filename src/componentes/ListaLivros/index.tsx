8a909579351c2bdb56006f6a42f27b015c9218e0
natcurti <naticurti01@gmail.com>
2024-06-16 16:10:14 -0300

    Update component to list books using apollo client

--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -1,24 +1,70 @@
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { ICategoria } from "../../interfaces/ICategoria";
-import { obterLivros } from "../../http";
+import { ILivro } from "../../interfaces/ILivro";
 import CardLivro from "../CardLivro";
 import "./ListaLivros.css";
+import { gql, useQuery } from "@apollo/client";
+import { AbBotao, AbCampoTexto } from "ds-alurabooks";
 
 interface ListaLivrosProps {
   categoria: ICategoria;
 }
 
+const OBTER_LIVROS = gql`
+  query ObterLivros($categoriaId: Int, $titulo: String) {
+    livros(categoriaId: $categoriaId, titulo: $titulo) {
+      id
+      slug
+      titulo
+      imagemCapa
+      opcoesCompra {
+        id
+        preco
+      }
+    }
+  }
+`;
+
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
-  const { data: livros } = useQuery({
-    queryKey: ["buscarLivrosDaCategoria", categoria],
-    queryFn: () => obterLivros(categoria),
+  const [busca, setBusca] = useState("");
+
+  const { data, refetch } = useQuery<{ livros: ILivro[] }>(OBTER_LIVROS, {
+    variables: {
+      categoriaId: categoria.id,
+    },
   });
 
+  const buscarLivros = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (busca) {
+      refetch({
+        categoriaId: categoria.id,
+        titulo: busca,
+      });
+    }
+  };
+
   return (
-    <section className="livros">
-      {livros?.map((livro) => (
-        <CardLivro livro={livro} key={livro.id} />
-      ))}
+    <section>
+      <form
+        onSubmit={buscarLivros}
+        style={{ maxWidth: "80%", margin: "0 auto", textAlign: "center" }}
+      >
+        <AbCampoTexto
+          value={busca}
+          onChange={setBusca}
+          placeholder="Digite o título do livro"
+        />
+        <div style={{ marginTop: "16px" }}>
+          <AbBotao texto="Buscar" />
+        </div>
+      </form>
+      <div className="livros">
+        {data?.livros.map((livro) => (
+          <CardLivro livro={livro} key={livro.id} />
+        ))}
+      </div>
     </section>
   );
 };
