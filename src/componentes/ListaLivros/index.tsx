831c2ec668a95962c7a81efbe40218df8659c96d
natcurti <naticurti01@gmail.com>
2024-06-17 18:36:58 -0300

    Use reactive vars to apply filter on books

--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ICategoria } from "../../interfaces/ICategoria";
 import CardLivro from "../CardLivro";
 import "./ListaLivros.css";
-import { AbBotao, AbCampoTexto } from "ds-alurabooks";
+import { AbCampoTexto } from "ds-alurabooks";
 import { useLivros } from "../../graphql/livros/hooks";
 import { useReactiveVar } from "@apollo/client";
-import { livrosVar } from "../../graphql/livros/state";
+import { filtroDeLivrosVar, livrosVar } from "../../graphql/livros/state";
 
 interface ListaLivrosProps {
   categoria: ICategoria;
@@ -14,36 +14,29 @@ interface ListaLivrosProps {
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
   const [busca, setBusca] = useState("");
 
-  // const { data, refetch } = useLivros(categoria);
-  useLivros(categoria);
+  useEffect(() => {
+    filtroDeLivrosVar({
+      ...filtroDeLivrosVar(),
+      titulo: busca.length >= 3 ? busca : "",
+    });
+  }, [busca]);
+
+  filtroDeLivrosVar({
+    ...filtroDeLivrosVar(),
+    categoria: categoria,
+  });
+  useLivros();
 
   const livros = useReactiveVar(livrosVar);
 
-  const buscarLivros = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // if (busca) {
-    //   refetch({
-    //     categoriaId: categoria.id,
-    //     titulo: busca,
-    //   });
-    // }
-  };
-
   return (
     <section>
-      <form
-        onSubmit={buscarLivros}
-        style={{ maxWidth: "80%", margin: "0 auto", textAlign: "center" }}
-      >
+      <form style={{ maxWidth: "80%", margin: "0 auto", textAlign: "center" }}>
         <AbCampoTexto
           value={busca}
           onChange={setBusca}
           placeholder="Digite o título do livro"
         />
-        <div style={{ marginTop: "16px" }}>
-          <AbBotao texto="Buscar" />
-        </div>
       </form>
       <div className="livros">
         {livros.map((livro) => (
--- src/graphql/livros/hooks.ts
+++ src/graphql/livros/hooks.ts
@@ -1,13 +1,15 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, useReactiveVar } from "@apollo/client";
 import { ILivro } from "../../interfaces/ILivro";
 import { OBTER_LIVROS } from "./queries";
-import { ICategoria } from "../../interfaces/ICategoria";
-import { livrosVar } from "./state";
+import { filtroDeLivrosVar, livrosVar } from "./state";
 
-export const useLivros = (categoria: ICategoria) => {
+export const useLivros = () => {
+  const filtro = useReactiveVar(filtroDeLivrosVar);
+
   return useQuery<{ livros: ILivro[] }>(OBTER_LIVROS, {
     variables: {
-      categoriaId: categoria.id,
+      categoriaId: filtro.categoria?.id,
+      titulo: filtro.titulo,
     },
     onCompleted(data) {
       if (data.livros) {
